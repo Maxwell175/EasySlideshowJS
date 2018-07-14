397924dc3b5135248138e0a778818ad35cec25f7
Maxwell Dreyster <admin@mdtech.us>
2018-07-13 22:41:03 -0400

    Update Copyright year.

--- EasySlideshow.js
+++ EasySlideshow.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2017 Maxwell Dreytser
+ * @license Copyright (c) 2018 Maxwell Dreytser
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
